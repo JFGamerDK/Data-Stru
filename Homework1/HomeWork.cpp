020f54e37d989e39263952b1ed647d3dd1c06ad8
JFGamerDK <greatliu1002@gmail.com>
2024-10-20 14:30:47 +0800

    10/20 14:30
    2 done

--- Homework1/HomeWork.cpp
+++ Homework1/HomeWork.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+#include <string>
 using namespace std;
 long long t=1;
 long long arrack[5][10000];
@@ -49,8 +51,22 @@ long long aryackmanfunction(int m,int n)//採用矩陣優化
     }
     return arrack[m][n];
 }
+void generatesubsets(vector<string>&s, vector<string>&nowSet, int index, vector<vector<string>>&allsubsets)
+{
+    allsubsets.push_back(nowSet);
+    for (int i = index; i < s.size(); ++i) 
+    {
+        nowSet.push_back(s[i]);// 将当前元素加入当前子集
+        generatesubsets(s, nowSet, i + 1, allsubsets);// 递归生成包含当前元素的子集
+        nowSet.pop_back();// 回溯，移除当前元素，继续生成其他子集
+    }
+}
 int main()
 {
+    int num=0;
+    vector<string> s;
+    vector<vector<string>> allsubsets;
+    vector<string> nowSet;
     for (int i = 0; i < 5; i++)//初始化矩陣元素皆為-1
     {
         for (int j = 0; j < 10000; j++) 
@@ -58,11 +74,30 @@ int main()
             arrack[i][j] = -1;
         }    
     }
-    int num=0,m=0,n=0;
+    int m=0,n=0;
     cout<<"輸入阿克曼函數(m,n)數值\n";
     cin>>m>>n;
     cout<<"m= "<<m<<"  n= "<<n<<endl;
+
+    cout << "請输入集合中的元素,元素可以是數字或是字母,以空格分隔,結束輸入按下Enter: ";
+    string input;
+    while (cin >> input) 
+    {
+        s.push_back(input);
+        if (cin.peek() == '\n') break; // 按下Enter結束輸入
+    }
+    generatesubsets(s, nowSet, 0, allsubsets);
+    cout<<"所有子集合為: "<<endl;
+    for (const auto& subset : allsubsets) 
+    {
+        cout << "{ ";
+        for (const auto& elem : subset) 
+        {
+            cout << elem << " ";
+        }
+        cout << "}" << endl;
+    }
     int rem=m,ren=n;
-    cout<<"Ackermann's function = "<< ackermanfunction(m,n) <<" 函數計算次數為 "<<t<<endl;t=0;
-    cout<<"Array Ackermann's function = "<< aryackmanfunction(rem,ren) <<"  函數計算次數為 "<<t<<endl;
+    cout<<"Ackermann's function = "<< ackermanfunction(m,n) <<" ,函數計算次數為 "<<t<<endl;t=0;
+    cout<<"Array Ackermann's function = "<< aryackmanfunction(rem,ren) <<"  ,函數計算次數為 "<<t<<endl;
 }
